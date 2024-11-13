9a597d0fde842c96589516cfa83afafaa35e147c
Will Stokes <wstokes@gmail.com>
2024-11-12 20:50:15 -0500

    disabling op modes we don't need now that we have tested them

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GiveUpSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GiveUpSample.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.enums.Team;
 
 @Autonomous( name = "Give Up Sample", group = "13702" )
+@Disabled
 public class GiveUpSample extends AbstractAutonomousOpMode
 {
   public GiveUpSample()
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GrabSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GrabSample.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.enums.Team;
 
 @Autonomous( name = "Grab Sample", group = "13702" )
+@Disabled
 public class GrabSample extends AbstractAutonomousOpMode
 {
   public GrabSample()
