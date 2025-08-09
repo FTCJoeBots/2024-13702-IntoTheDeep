e762e86a7c0042885711c5e6784b1006014b8567
tommystokes <17284@stjoebears.com>
2025-08-09 13:24:13 -0400

    Loaded images and cycled through them or exit/quit
    JoePaint Version 0.2

--- Limelight/JoePaint.py
+++ Limelight/JoePaint.py
@@ -1,17 +1,49 @@
-import math
-from enum import Enum
-import random
+from tkinter.font import names
 
 import cv2
-import numpy as np
+import glob
+#===========================================================
+def showImage():
+    cv2.imshow("Joe Paint!!", imageHolder[currentImage])
 
-def showImage(title, image):
-    cv2.imshow(title, image)
-    cv2.waitKey(0)
+#===========================================================
+fileNames = glob.glob('snapshots/*.png')
+imageHolder = []
+for fileName in fileNames:
+    imageHolder.append( cv2.imread(fileName) )
+
+currentImage = 0
+showImage()
+
+lastKeyPressed = None
+while True:
+    keyPressed = cv2.waitKey(1) & 0xFF
+
+    if keyPressed== 2:
+        currentImage -= 1
+        if currentImage < 0:
+            currentImage = len(imageHolder)-1
+        showImage()
+
+    if keyPressed == 3:
+        currentImage += 1
+        if currentImage == len(imageHolder):
+            currentImage = 0
+        showImage()
+
+    #Escape or Q exits Joe Paint!!!
+    if keyPressed == 27 or keyPressed == ord('q'):
+        break
+
+    if keyPressed != lastKeyPressed:
+        lastKeyPressed = keyPressed
+        if keyPressed!= 255:
+            print(keyPressed)
+
 
 #image = cv2.imread('yellowSample.png')
 #image = cv2.imread('redSample.png')
-image = cv2.imread('snapshots/blueSample.png')
+##image = cv2.imread('snapshots/blueSample.png')
 #image = cv2.imread('yellowBest2.png')
-showImage('Joe Paint!', image)
+##showImage('Joe Paint!', image)
 
